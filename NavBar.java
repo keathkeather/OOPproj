e8f63d53180f5ba3d55e7c9d23d5b79badb2b4d3
Simoun Tompar <simouncloyd.tompar@edu.cit>
2023-05-26 11:27:47 +0800

    Updated NavBar buttons

--- NavBar.java
+++ NavBar.java
@@ -11,78 +11,64 @@ package com.mycompany.navbar;
 import javax.swing.*;
 import java.awt.*;
 
-public class NavBar extends JFrame {
+public class NavBar extends JPanel {
     public NavBar() {
-        setTitle("Bank System");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(1080, 720);
-        getContentPane().setBackground(Color.decode("#FFFFFF"));
-
-        // Color Theme
-        Color primaryColor = Color.decode("#005176");
-        Color secondaryColor = Color.decode("#4F93D2");
-        Color tertiaryColor = Color.decode("#5CBFE9");
-        Color whiteColor = Color.decode("#FDFFFF");
-        Color backgroundColor = Color.decode("#FFFFFF");
-
-        JPanel sidebarPanel = new JPanel();
-        sidebarPanel.setBackground(primaryColor);
-        sidebarPanel.setLayout(new BoxLayout(sidebarPanel, BoxLayout.Y_AXIS));
+        setBackground(Color.decode("#005176"));
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
         JLabel titleLabel = new JLabel("Navigation");
-        titleLabel.setForeground(whiteColor);
+        titleLabel.setForeground(Color.decode("#FDFFFF"));
         titleLabel.setFont(new Font("Arial", Font.BOLD, 20));
         titleLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
         titleLabel.setBorder(BorderFactory.createEmptyBorder(20, 0, 40, 0));
-        sidebarPanel.add(titleLabel);
+        add(titleLabel);
 
-        // Create navigation labels
         String[] navLabels = {"Client", "Send/Receive", "Checking Accounts", "Transactions",
                 "Loans", "Credit Cards", "Bill Payments", "Investments"};
 
-        // Creating a navBox for "Checking Accounts" to get its preferred size
         JPanel referenceBox = new JPanel();
         referenceBox.setLayout(new BorderLayout());
         referenceBox.setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20));
         JLabel referenceLabel = new JLabel("Checking Accounts");
-        referenceLabel.setFont(new Font("Arial", Font.PLAIN, 16));
+        referenceLabel.setFont(new Font("Arial", Font.PLAIN, 14)); 
         referenceBox.add(referenceLabel, BorderLayout.CENTER);
         Dimension referenceSize = referenceBox.getPreferredSize();
+        referenceSize.width += 35; 
 
         for (String label : navLabels) {
             JPanel navBox = new JPanel();
-            navBox.setBackground(secondaryColor);
+            navBox.setBackground(Color.decode("#4F93D2"));
             navBox.setLayout(new BorderLayout());
             navBox.setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20));
-            navBox.setPreferredSize(referenceSize); // Set the preferred size to the referenceSize
+            navBox.setPreferredSize(referenceSize);
 
-            JLabel navLabel = new JLabel(label);
-            navLabel.setForeground(whiteColor);
-            navLabel.setFont(new Font("Arial", Font.PLAIN, 16));
-            navBox.add(navLabel, BorderLayout.CENTER);
+            JButton navButton = new JButton(label);
+            navButton.setForeground(Color.decode("#FDFFFF"));
+            navButton.setFont(new Font("Arial", Font.PLAIN, 14));
+            navButton.setBackground(Color.decode("#4F93D2"));
+            navButton.setBorderPainted(false);
+            navButton.setFocusPainted(false);
+            navBox.add(navButton, BorderLayout.CENTER);
 
             JPanel boxContainer = new JPanel();
             boxContainer.setLayout(new FlowLayout(FlowLayout.CENTER));
-            boxContainer.setBackground(primaryColor);
+            boxContainer.setBackground(Color.decode("#005176"));
             boxContainer.add(navBox);
 
-            sidebarPanel.add(boxContainer);
+            add(boxContainer);
         }
 
-        JScrollPane scrollPane = new JScrollPane(sidebarPanel);
-        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-
-        getContentPane().add(scrollPane, BorderLayout.WEST);
+        setPreferredSize(new Dimension(200, 600));
     }
-
     public static void main(String[] args) {
-        EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new NavBar().setVisible(true);
-            }
-        });
+    JFrame frame = new JFrame();
+    frame.add(new NavBar());
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setSize(300, 300);
+    frame.setVisible(true);
     }
 }
+
 
 
 
